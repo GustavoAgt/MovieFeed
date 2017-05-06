e0fdd9566dedf35736ff8c80d83239729633ab0f
GustavoAgt <gustavo.adolf.gomez@gmail.com>
2017-05-06 14:15:50 -0400

    Login UI advanced  50%, username and password field added, logotype added.

--- app/src/main/java/com/ggproject/gustavo/moviefeed/LoginActivity.java
+++ app/src/main/java/com/ggproject/gustavo/moviefeed/LoginActivity.java
@@ -1,7 +1,9 @@
 package com.ggproject.gustavo.moviefeed;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        TextView logoType = (TextView) findViewById(R.id.logoTypeLogin);
+        Typeface pacifico = Typeface.createFromAsset(getAssets(), "fonts/Pacifico-Regular.ttf");
+        logoType.setTypeface(pacifico);
+
     }
+
+
 }
