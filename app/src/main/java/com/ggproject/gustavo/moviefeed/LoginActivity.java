77e34bfe299e11d3c8cd247cafbcbe358fc1d4fd
GustavoAgt <gustavo.adolf.gomez@gmail.com>
2017-05-07 12:02:07 -0400

    Login button changes.

--- app/src/main/java/com/ggproject/gustavo/moviefeed/LoginActivity.java
+++ app/src/main/java/com/ggproject/gustavo/moviefeed/LoginActivity.java
@@ -1,8 +1,13 @@
 package com.ggproject.gustavo.moviefeed;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
@@ -12,11 +17,29 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+
         TextView logoType = (TextView) findViewById(R.id.logoTypeLogin);
         Typeface pacifico = Typeface.createFromAsset(getAssets(), "fonts/Pacifico-Regular.ttf");
         logoType.setTypeface(pacifico);
 
+        loginButtonEffect();
+        clickLoginButtonEffect();
     }
 
 
+    private void loginButtonEffect(){
+        final Button loginButton = (Button) findViewById(R.id.loginUser);
+
+    }
+
+    private void clickLoginButtonEffect(){
+        final Button loginButton = (Button) findViewById(R.id.loginUser);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(),MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
 }
